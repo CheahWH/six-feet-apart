ed490fa2023166df58fb1d831db4e79ea15290e1
CheahWH <78494351+CheahWH@users.noreply.github.com>
2022-04-12 17:22:02 -0700

    prints topbar

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,8 @@
+import Topbar from "../../components/topbar/Topbar";
+
 export default function Home() {
     return (
-        <div>
-            this is home
-        </div>
+        <Topbar/>
     )
 }
  
