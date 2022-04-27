8ad9b5bddd3419c0c19b97a1f2df7505bbcd1930
CheahWH <78494351+CheahWH@users.noreply.github.com>
2022-04-26 19:44:04 -0700

    Feed (Share, Post Components)
    completed

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,8 +1,40 @@
 import "./share.css"
+import {PermMedia, Label, Room, EmojiEmotions} from "@mui/icons-material"
+
 export default function Share() {
     return (
-        <div>
-            hellooooo its share
+        <div className = "share">
+            <div className="shareWrapper">
+                <div className="shareTop">
+                    <img className="shareProfileImg" src="/assets/person/1.jpeg" alt = ""/>
+                    <input 
+                        placeholder="What's up?" 
+                        className = "shareInput"
+                    />
+                </div>
+                <hr className="shareHr"/>
+                <div className="shareBottom">
+                    <div className="shareOptions">
+                        <div className="shareOption">
+                            <PermMedia htmlColor="#501bb1" className = "shareIcon"/>
+                            <span className = "shareOptionText">Photo or Video</span>
+                        </div>
+                        <div className="shareOption">
+                            <Label htmlColor="#501bb1" className = "shareIcon"/>
+                            <span className = "shareOptionText">Tag</span>
+                        </div>
+                        <div className="shareOption">
+                            <Room htmlColor="#501bb1" className = "shareIcon"/>
+                            <span className = "shareOptionText">Location</span>
+                        </div>
+                        <div className="shareOption">
+                            <EmojiEmotions htmlColor="#501bb1" className = "shareIcon"/>
+                            <span className = "shareOptionText">Feelings</span>
+                        </div>
+                        <button className ="shareButton">Share!</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
