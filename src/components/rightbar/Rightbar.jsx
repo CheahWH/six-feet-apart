389b82f5a7606259417a706b99258da3b190b623
CheahWH <78494351+CheahWH@users.noreply.github.com>
2022-05-01 17:41:51 -0700

    Profile Page completed

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,11 +2,11 @@ import "./rightbar.css"
 import Online from "../online/Online"
 import {Users} from "../../dummyData"
 
-export default function Rightbar() {
-    return (
-        <div className = "rightbar">
-            <div className="rightbarWrapper">
-                <div className="birthdayContainer">
+export default function Rightbar({profile}) {
+    const HomeRightbar = () =>{
+        return(
+            <>
+            <div className="birthdayContainer">
                     <img className="birthdayImg" src="assets/gift.png" alt="" />
                     <span className="birthdayText">
                         <b>Donkey Doug</b> and <b>3 other friends</b> have a birthday today!
@@ -19,6 +19,61 @@ export default function Rightbar() {
                         <Online key={u.id} user={u}/>
                     ))}
                 </ul>
+            </>
+        )
+    }
+    const ProfileRightbar = () =>{
+        return (
+            <>
+            <h4 className="rightbarTitle">User information</h4>
+            <div className="rightbarInfo">
+                <div className="rightbarInfoItem">
+                    <span className="rightbarInfoKey">College: </span>
+                    <span className="rightbarInfoValue">San Jose State University</span>
+                </div>
+                <div className="rightbarInfoItem">
+                    <span className="rightbarInfoKey">Year: </span>
+                    <span className="rightbarInfoValue">Junior</span>
+                </div>
+                <div className="rightbarInfoItem">
+                    <span className="rightbarInfoKey">From: </span>
+                    <span className="rightbarInfoValue">Malaysia</span>
+                </div>
+            </div>
+            <h4 className="rightbarTitle">User friends</h4>
+            <div className="rightbarFollowings">
+                <div className="rightbarFollowing">
+                    <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg"/>
+                    <span className="rightbarFollowingName">John Carter</span>
+                </div>
+                <div className="rightbarFollowing">
+                    <img src="assets/person/2.jpeg" alt="" className="rightbarFollowingImg"/>
+                    <span className="rightbarFollowingName">John Carter</span>
+                </div>
+                <div className="rightbarFollowing">
+                    <img src="assets/person/3.jpeg" alt="" className="rightbarFollowingImg"/>
+                    <span className="rightbarFollowingName">John Carter</span>
+                </div>
+                <div className="rightbarFollowing">
+                    <img src="assets/person/4.jpeg" alt="" className="rightbarFollowingImg"/>
+                    <span className="rightbarFollowingName">John Carter</span>
+                </div>
+                <div className="rightbarFollowing">
+                    <img src="assets/person/5.jpeg" alt="" className="rightbarFollowingImg"/>
+                    <span className="rightbarFollowingName">John Carter</span>
+                </div>
+                <div className="rightbarFollowing">
+                    <img src="assets/person/6.jpeg" alt="" className="rightbarFollowingImg"/>
+                    <span className="rightbarFollowingName">John Carter</span>
+                </div>
+            </div>  
+            </>
+        )
+    }
+    return (
+        <div className = "rightbar">
+            <div className="rightbarWrapper">
+                <ProfileRightbar/>
             </div>
         </div>
     )
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
 import "./sidebar.css"
-import {RssFeed,
+import {
+    RssFeed,
     Chat, 
     VideoLibrary, 
     Groups,
@@ -8,6 +9,7 @@ import {RssFeed,
     Work,
     Event, 
     School,
+    SchoolSharp,
 } from "@mui/icons-material"
 import {Users} from "../../dummyData"
 import CloseFriend from "../closeFriend/CloseFriend";
@@ -21,6 +23,38 @@ export default function Sidebar() {
                     <li className="sidebarListItem">
                         <RssFeed className="sidebarIcon"/>
                         <span className="sidebarListItem">Feed</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Chat className="sidebarIcon"/>
+                        <span className="sidebarListItem">Chats</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <VideoLibrary className="sidebarIcon"/>
+                        <span className="sidebarListItem">Videos</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Groups className="sidebarIcon"/>
+                        <span className="sidebarListItem">Groups</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Bookmarks className="sidebarIcon"/>
+                        <span className="sidebarListItem">Bookmarks</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <QuestionMark className="sidebarIcon"/>
+                        <span className="sidebarListItem">Questions</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Work className="sidebarIcon"/>
+                        <span className="sidebarListItem">Jobs</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Event className="sidebarIcon"/>
+                        <span className="sidebarListItem">Events</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <School className="sidebarIcon"/>
+                        <span className="sidebarListItem">Courses</span>
                     </li>
                 </ul>
                 <button className="sidebarButton">Show More</button>
